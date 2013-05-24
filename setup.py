7224f157a0c387376618b5079656757f02fe26a5
Mauricio Souza Lima <mauricio.lima@corp.globo.com>
2013-05-24 00:51:22 -0300

    initial commit

---
+++ setup.py
@@ -0,0 +1,16 @@
+# coding: utf-8
+from setuptools import setup, find_packages
+
+version = '0.1'
+
+setup(
+    name = 'torsche',
+    version = '0.0.1',
+    description = 'Schematic models for tornado',
+    author = 'Arkanjuca',
+    author_email = 'arkanjuca@gmail.com',
+    url='http://github.com/mauriciosl/torsche',
+    packages = find_packages(),
+    zip_safe = False,
+    include_package_data = True,
+)
